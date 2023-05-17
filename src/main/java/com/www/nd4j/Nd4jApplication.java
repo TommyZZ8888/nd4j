92267909932af761c1392afea9877d6e2f5c200b
TommyZZ8888 <1950442495@qq.com>
2023-05-17 09:33:20 +0800

    深度学习搭建

---
+++ src/main/java/com/www/nd4j/Nd4jApplication.java
@@ -0,0 +1,13 @@
+package com.www.nd4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nd4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nd4jApplication.class, args);
+    }
+
+}
